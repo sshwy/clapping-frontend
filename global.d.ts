f3611c876a7b8856178aab48280aad0596c40522
sshwy <jy.cat@qq.com>
2021-05-20 16:52:31 +0800

    feature: add global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,40 @@
+import { Socket } from "socket.io-client";
+import { Action, Getter, Mutation, Store } from "vuex";
+
+interface AppState {
+  socket: Socket;
+  authorized: boolean;
+  userID: string;
+  username: string;
+  // version: string; // 前端版本号
+  backend_version: string; // 后端版本
+  games: Game[]; // 游戏数组
+  scene_type: "unauthorized" | "room_list" | "room_info" | "gaming"; // 场景
+  data: GamingData | {}; // 不同的 scene_type 对应不同的 data
+  messages: Message[]; // 左下角提示信息列表
+}
+
+interface Message {
+  id: string;
+  text: string;
+  type: "info" | "success" | "error";
+}
+
+interface GamingData {
+  game_id: number;
+}
+
+type SocketMutations = {
+  [key: string]: Mutation<AppState>;
+};
+
+type AppActions = {
+  [key: string]: Action<AppState, AppState>;
+};
+
+type AppGetters = {
+  [key: string]: Getter<AppState, AppState>;
+};
+
+type AppStore = Store<AppState>;
+type Game = any;
